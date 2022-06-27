4f09c0025b3e23e633eb64d8e3ded22f5ebd8f03
fazilajafarli <fezile.ceferli@gmail.com>
2022-06-27 16:17:09 +0400

    news search added

--- assistant.py
+++ assistant.py
@@ -10,28 +10,27 @@ import random
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-
 wikipedia.set_lang("en")
 
 def speak(audio):
-    engine.say(audio)   
+    engine.say(audio) 
     engine.runAndWait()
 
 def command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Jinn listening...")
+        print("Genie listening...")
         audio=r.listen(source)
         try:    
             query = r.recognize_google(audio)
             return query
         except:
-            print("Didn't hear well, say again")
+            print("I didn't hear you well, master.")
 
 while True:
     query = command().lower()
     if 'name' in query:
-        speak("Hello! My name is Jinn, and what is your name?")
+        speak("Hello! My name is Genie, master!")
     elif 'time' in query:
         time = datetime.datetime.now().strftime('%I:%M')
         speak(f"It's {time}")
@@ -41,8 +40,31 @@ while True:
     elif 'what is' in query:
         query = query.replace('what is',"")
         speak(wikipedia.summary(query,2))
+    elif 'news' in query:
+            def trendnews(): 
+                url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=59ff055b7c754a10a1f8afb4583ef1ab"
+                page = requests.get(url).json() 
+                article = page["articles"] 
+                results = [] 
+                for ar in article: 
+                    results.append(ar["title"]) 
+                for i in range(len(results)): 
+                    print(i + 1, results[i]) 
+                speak("Here are the top trending news...")
+                speak("Do yo want me to read?")
+                reply = command().lower()
+                reply = str(reply)
+                if reply == "yes":
+                    speak(results)
+                else:
+                    speak('ok!')
+                    pass
+            trendnews() 
+
+
+
     elif "bye" in query:
-        speak("Have a nice day! ")
+        speak("Bye-bye master!")
         break
     else:
-        speak("I don't understand what you are saying")
+        speak("I don't understand what you are saying, master.")
